49579723ec7dd174c827c6384ec38397467a697e
ChenYouhua <chenyouhua@spaceon.com>
2019-08-20 19:53:30 +0800

    提交创建readme.md脚本

--- script/python/create_md.py
+++ script/python/create_md.py
@@ -1,9 +1,22 @@
+#!/usr/bin/python3
 import os
+from pathlib import Path
 
+
+    
+def getCurDirName():
+    """
+    :return: 获取当前执行目录
+    """
+    p=Path(os.getcwd())
+    return p.name
+    
 def create_md():
+    dirName = getCurDirName()
+    f = open(dirName + '.md','w')
+    f.write('#!/user/bin/python')
+    f.close()
     
     
-def getCurDir():
-    return os.curdir
-    
-print getCurDir()
+print(getCurDirName())
+create_md()
