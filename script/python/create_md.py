c659d65b7f6466646385aa5541d95343952ce414
ChenYouhua <chenyouhua@spaceon.com>
2019-08-22 10:30:14 +0800

    优化

--- script/python/create_md.py
+++ script/python/create_md.py
@@ -3,27 +3,30 @@ import os
 from pathlib import Path
 
 
-    
-def getCurDirName():
+def get_cur_dir_name():
     """
     :return: 获取当前执行目录
     """
-    p=Path(os.getcwd())
+    p = Path(os.getcwd())
     return p.name
-    
+
+
 def create_md():
-    dirName = getCurDirName()
-    print(dirName)
-    mdFileName = dirName + '.md'
-    if (os.path.exists(mdFileName)):
+    dir_name = get_cur_dir_name()
+    if len(dir_name) == 0 : 
+        print('目录为空，使用默认文件名')
+        dir_name = 'README'
+    print(dir_name)
+    md_name = dir_name + '.md'
+    if os.path.exists(md_name):
         print('文件已经存在')
         return
 
-    f = open(dirName + '.md','w')
-    f.write('#!/user/bin/python')
+    f = open(dir_name + '.md', 'w')
+    f.write('# '+dir_name)
     f.close()
     return '文件创建成功'
-    
+
 
 create_md()
 
