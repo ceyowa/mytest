84d580b708b66d5fcbb444b732d4cc1801604a02
ChenYouhua <chenyouhua@spaceon.com>
2021-04-10 11:52:15 +0800

    优化

--- script/python/huawei_questions.py
+++ script/python/huawei_questions.py
@@ -70,6 +70,11 @@ class Question:
     def append_option_content(self, new_line):
         self.options[len(self.options) - 1].append_value(new_line)
 
+    def get_answer_value(self):
+        for ch in self.answers:
+            for item in self.options:
+                if item.optionNum == ch:
+                    return item.value
 
 WAIT_QUESTION = 0
 '''等待问题开始'''
@@ -84,6 +89,8 @@ DIFFICULTIES = ['一般', '容易', '困难']
 def write_to_excel(all_questions, excel_path):
     rows = []
     for item in all_questions:
+        if not item.answers:
+            continue
         row = [
             '知识竞赛',
             '多选题' if item.multi else '单选题',
@@ -94,10 +101,45 @@ def write_to_excel(all_questions, excel_path):
             json.dumps(item.options, default=json_encoder),
             '',
             item.answers,
-            '',
-            ''
+            '1',
+            '1'
         ]
         rows.append(row)
+        if not item.multi:
+            answer_value = item.get_answer_value()
+            if answer_value and len(answer_value) < 12:
+                if len(item.options) == 2 and (answer_value == '正确' or answer_value == '错误'):
+                    row = [
+                        '知识竞赛',
+                        '判断题',
+                        '文本题',
+                        random.choice(DIFFICULTIES),
+                        '华为,H12',
+                        item.question + '\u000D\u000A',
+                        # 选项
+                        json.dumps(item.options, default=json_encoder),
+                        '',
+                        answer_value,
+                        '1',
+                        '1'
+                    ]
+                    rows.append(row)
+                else:
+                    row = [
+                        '知识竞赛',
+                        '填空题',
+                        '文本题',
+                        random.choice(DIFFICULTIES),
+                        '华为,H12',
+                        item.question + '\u000D\u000A',
+                        # 选项
+                        '',
+                        '',
+                        answer_value,
+                        '2',
+                        '2'
+                    ]
+                    rows.append(row)
 
     df = pd.DataFrame(rows,
                       columns=['应用类型', '试题类型', '题型', '难易度', '标签', '题干', '选项', '文件地址', '答案', '正确得分', '错误扣分'])
@@ -105,15 +147,7 @@ def write_to_excel(all_questions, excel_path):
     pass
 
 
-def parse_questions():
-    # txt_path = "H12-261 451题-已解锁.txt"
-    # excel_path = r"H12-261 451题-已解锁.xlsx"
-    # expect_len = 451
-    # tags = "华为,H12"
-    txt_path = "华为H12-261 HCIE-RS题库.txt"
-    excel_path = r"华为H12-261 HCIE-RS题库.xlsx"
-    expect_len = 261
-    tags = "华为,H12,HCIE-RS"
+def parse_questions(txt_path, excel_path, expect_len, tags):
     q_start_re = re.compile(r'^QUESTION (\d+)$')
     choose_re = re.compile(r'^([A-Z])\.(.*)$')
     answers_re = re.compile(r'^((Correct Answer)|(CorrectAnswer)):\s*(.*)\s*$')
@@ -164,7 +198,6 @@ def parse_questions():
                                 # 选项内容
                                 question.append_option_content(line)
 
-
     length = len(all_questions)
     print(length)
     if expect_len != length:
@@ -212,7 +245,8 @@ if __name__ == '__main__':
     # frame.to_excel(xl_writer, index=False)  # 写到文件中
 
     try:
-        parse_questions()
+        parse_questions("H12-261 451题-已解锁.txt", r"H12-261 451题-已解锁.xlsx", 451, "华为,H12")
+        parse_questions("华为H12-261 HCIE-RS题库.txt", r"华为H12-261 HCIE-RS题库.xlsx", 261, "华为,H12,HCIE-RS")
     except Exception as e:
         print(str(e))
         raise
